b3a24e8f371c12e41b8aa7a07cc018ee485bf3d2
Minh Truong Le <108687796+minhtruong0603@users.noreply.github.com>
2024-09-04 19:06:23 +0700

    Add files via upload

---
+++ devops-practice/start.sh
@@ -0,0 +1,7 @@
+kubectl apply -f Deployment.yml
+kubectl apply -f DaemonSet.yml
+kubectl apply -f ReplicaSet.yml
+kubectl apply -f DeploymentService.yml
+kubectl apply -f StatefulSet.yml
+kubectl apply -f Job.yml
+kubectl apply -f CronJob.yml
---
+++ devops-practice/stop.sh
@@ -0,0 +1,7 @@
+kubectl delete -f Deployment.yml
+kubectl delete -f DaemonSet.yml
+kubectl delete -f ReplicaSet.yml
+kubectl delete -f DeploymentService.yml
+kubectl delete -f StatefulSet.yml
+kubectl delete -f Job.yml
+kubectl delete -f CronJob.yml7
